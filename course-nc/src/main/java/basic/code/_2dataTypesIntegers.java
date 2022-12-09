7879092193c0190deae1741f59740dbae6bcfecc
Andres Giraldo Arenas <andresganc@gmail.com>
2022-12-09 12:26:48 -0500

    New: Data Types Integers & Decimals

---
+++ course-nc/src/main/java/basic/code/_2dataTypesIntegers.java
@@ -0,0 +1,34 @@
+package basic.code;
+
+public class _2dataTypesIntegers {
+    public static void main(String[] args){
+
+        // DATA TYPES - PRIMITIVES- INTEGERS
+
+        // BYTE
+        // Space: 1 Byte
+        // Range: From -128 to 127
+        Byte valByte = 5;
+        System.out.println("Byte: " + valByte);
+
+        // INT
+        // Space: 4 Bytes space
+        // Range: From -2.147.483.648 to 2.147.483.647
+        Integer valInt = 5;
+        System.out.println("Int: " + valInt);
+
+        // SHORT
+        // Space: 2 Bytes space
+        // Range: From -32.768 to .32.767
+        Short valShort = 5;
+        System.out.println("Short: " + valShort);
+
+        // LONG
+        // Space: 8 Bytes
+        // Range: Very Long
+        // Suffix: L
+        Long valLong = 5L;
+        System.out.println("Long: " + valLong);
+
+    }
+}
---
+++ course-nc/src/main/java/basic/code/_3dataTypesDecimal.java
@@ -0,0 +1,21 @@
+package basic.code;
+
+public class _3dataTypesDecimal {
+    public static void main(String[] args) {
+
+        // DATA TYPES - PRIMITIVES- DECIMALS
+
+        // FLOAT
+        // Space: 4 Bytes
+        // Range: Approx 6 or 7 decimal numbers
+        // Suffix: F
+        Float valFloat = 5.13458F;
+        System.out.println("Float: " + valFloat);
+
+        // DOUBLE
+        // Space: 8 Bytes
+        // Range: Approx 15 decimal numbers
+        Double valDouble = 5.1874565798465;
+        System.out.println("Double: " + valDouble);
+    }
+}
